0b4a039b29ec128e1cc9ef69b3587c926accce62
兴武 <920738659@qq.com>
2020-03-14 16:40:06 +0800

    ...

--- 07_多次开方.js
+++ 07_多次开方.js
@@ -1,6 +1,6 @@
 // 第7题 【多次开方】请计算下面表达式的值，要求使用循环语句
- var a = Math.sqrt(2);
+ var ab = Math.sqrt(2);
  for (var i = 0; i < 4; i++) {
-     a = Math.sqrt(a / 2);
+     ab = Math.sqrt(ab / 2);
  }
- console.log(a);
+ console.log(ab);
